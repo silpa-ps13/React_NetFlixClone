aa56a669787bbda2d46b56413b6f100ea1726180
silpa-ps13 <48428790+silpa-ps13@users.noreply.github.com>
2023-06-26 21:01:10 +0530

    2 files modified, 2 added

--- assets.md
+++ assets.md
@@ -1,3 +1,5 @@
 netflix-logo='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png'
 
 nav-avatar='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'§
+
+banner='https://wallpaperaccess.com/full/2703652.png'
--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/navBar/NavBar';
+import Banner from './components/banner/Banner';
 export default function App() {
   return (
     <div>
       <NavBar></NavBar>
+      <Banner></Banner>
     </div>
   );
 }
