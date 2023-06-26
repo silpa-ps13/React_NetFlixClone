02837dcd21276806f65d1fe46ad3ffecf1ab2dcb
silpa-ps13 <48428790+silpa-ps13@users.noreply.github.com>
2023-06-26 21:44:03 +0530

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/navBar/NavBar';
 import Banner from './components/banner/Banner';
+import RowPost from './components/rowpost/RowPost';
 export default function App() {
   return (
     <div>
       <NavBar></NavBar>
       <Banner></Banner>
+      <RowPost></RowPost>
     </div>
   );
 }
